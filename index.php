ad89e4994c231bfff4e9c25a2cd4c12de3370914
WavestonePOC <jonathan.tomasini@wavestone.fr>
2016-10-14 16:36:00 +0200

    ,bv

--- index.php
+++ index.php
@@ -6,13 +6,19 @@ echo getenv("DATABASE_SERVICE_NAME");
 echo "\r\n";
 echo getenv("DATABASE_NAME");
 echo "\r\n";
-echo getenv("DATABASE_ENGINE");
-echo "\r\n";
 echo getenv("DATABASE_USER");
 echo "\r\n";
 echo getenv("DATABASE_PASSWORD");
 echo "\r\n";
 
+$connectionstring=getenv("DATABASE_SERVICE_NAME")." ".getenv("DATABASE_NAME")." ".getenv("DATABASE_USER")." ".getenv("DATABASE_PASSWORD");
+
+echo $connectionstring;
+echo "\r\n";
+
+echo $dbconn=pg_connect($connectionstring);
+
+echo "\r\n";
 
 try{
   $bdd = new PDO('mysql:host='.getenv("DATABASE_SERVICE_NAME").';dbname=default;charset=utf8', 'wavestoneadmin', 'Azerty123');
