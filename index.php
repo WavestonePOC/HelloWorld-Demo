213c32d6a16fbfa5d83ca4c33b11b2e6b0681dd3
WavestonePOC <jonathan.tomasini@wavestone.fr>
2016-10-14 13:57:40 +0200

    f

--- index.php
+++ index.php
@@ -3,14 +3,23 @@
 echo "Bonjour Tlm!V2\n";
 
 echo getenv("DATABASE_SERVICE_NAME");
-echo "\n";
+echo "\r\n";
 echo getenv("DATABASE_NAME");
-echo "\n";
+echo "\r\n";
 echo getenv("DATABASE_ENGINE");
-echo "\n";
+echo "\r\n";
 echo getenv("DATABASE_USER");
-echo "\n";
+echo "\r\n";
 echo getenv("DATABASE_PASSWORD");
+echo "\r\n";
+
+
+try{
+  $bdd = new PDO('mysql:host=pgsql;dbname=default;charset=utf8', 'wavestoneadmin', 'Azerty123');
+}catch (Exception $e){
+  die('Erreur : '.$e->getMessage());
+}
+
 
 
 ?>
