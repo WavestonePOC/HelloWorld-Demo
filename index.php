a7b49f9090ca6d8d9a121397915f01b959111352
WavestonePOC <jonathan.tomasini@wavestone.fr>
2016-10-14 13:29:19 +0200

    ajout \n

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
-echo "Bonjour Tlm!V2";
+echo "Bonjour Tlm!V2\n";
 
-echo getenv("DATABASE_SERVICE_NAME");
-echo getenv("DATABASE_ENGINE");
-echo getenv("DATABASE_NAME");
-echo getenv("DATABASE_USER");
-echo getenv("DATABASE_PASSWORD");
+echo getenv("DATABASE_SERVICE_NAME\n");
+echo getenv("DATABASE_NAME\n");
+echo getenv("DATABASE_ENGINE\n");
+echo getenv("DATABASE_USER\n");
+echo getenv("DATABASE_PASSWORD\n");
 
 
 ?>
