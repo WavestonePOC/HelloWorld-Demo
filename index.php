18cec5f2f9fbb13dd6e18e9e6ef450905c9067d1
WavestonePOC <jonathan.tomasini@wavestone.fr>
2016-10-31 17:27:47 +0100

    vcxv

--- index.php
+++ index.php
@@ -1,24 +1,37 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Titre de la page</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+
 <?php
 
-echo "Bonjour Tlm!V3\n";
+  echo "Bonjour Tlm!V3\n";
 
-echo getenv("DATABASE_SERVICE_NAME");
-echo "\r\n";
-echo getenv("DATABASE_NAME");
-echo "\r\n";
-echo getenv("DATABASE_USER");
-echo "\r\n";
-echo getenv("DATABASE_PASSWORD");
-echo "\r\n";
+  echo getenv("DATABASE_SERVICE_NAME");
+  echo "\r\n";
+  echo getenv("DATABASE_NAME");
+  echo "\r\n";
+  echo getenv("DATABASE_USER");
+  echo "\r\n";
+  echo getenv("DATABASE_PASSWORD");
+  echo "\r\n";
 
-$connectionstring='host='.getenv("DATABASE_SERVICE_NAME")." port=5432 dbname=".getenv("DATABASE_NAME")." user=".getenv("DATABASE_USER")." password=".getenv("DATABASE_PASSWORD");
+  $connectionstring='host='.getenv("DATABASE_SERVICE_NAME")." port=5432 dbname=".getenv("DATABASE_NAME")." user=".getenv("DATABASE_USER")." password=".getenv("DATABASE_PASSWORD");
 
-echo $connectionstring;
-echo "\r\n";
+  echo $connectionstring;
+  echo "\r\n";
 
-echo $dbconn=pg_connect($connectionstring);
+  echo $dbconn=pg_connect($connectionstring);
 
-echo "\r\n";
+  echo "\r\n";
 
 
 ?>
+
+</body>
+</html>
---
+++ index1.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "Bonjour Tlm!V3\n";
+
+echo getenv("DATABASE_SERVICE_NAME");
+echo "\r\n";
+echo getenv("DATABASE_NAME");
+echo "\r\n";
+echo getenv("DATABASE_USER");
+echo "\r\n";
+echo getenv("DATABASE_PASSWORD");
+echo "\r\n";
+
+$connectionstring='host='.getenv("DATABASE_SERVICE_NAME")." port=5432 dbname=".getenv("DATABASE_NAME")." user=".getenv("DATABASE_USER")." password=".getenv("DATABASE_PASSWORD");
+
+echo $connectionstring;
+echo "\r\n";
+
+echo $dbconn=pg_connect($connectionstring);
+
+echo "\r\n";
+
+
+?>
